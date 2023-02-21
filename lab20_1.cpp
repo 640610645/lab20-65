b383b326b860e47a93422eed2847f5fa8ff76385
640610645 <nontapan_chanadee@cmu.ac.th>
2023-02-21 20:48:25 +0700

    lab 20 completed

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect r1, Rect r2){
+    double leftR1 = r1.x, rightR1 = r1.x+r1.w, upperR1 = r1.y, lowerR1 = r1.y-r1.h;
+    double leftR2 = r2.x, rightR2 = r2.x+r2.w, upperR2 = r2.y, lowerR2 = r2.y-r2.h;
+    double leftLap = max(leftR1,leftR2), rightLap = min(rightR1,rightR2), upperLap = min(upperR1,upperR2), lowerLap = max(lowerR1,lowerR2);
+	double width = rightLap-leftLap, height = upperLap-lowerLap;
+	double area = width*height;
+	if(width <= 0 || height <= 0) return 0;
+	else return area;
+}
 
-
-
+int main(){
+	Rect R1 = {1,1,2,2};
+	Rect R2 = {6,6,1,1};	
+	cout << overlap(R1,R2);	
 }
